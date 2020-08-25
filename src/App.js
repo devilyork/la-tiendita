a489cbab83cab9f85fcbbaf56912b2a58241e8c3
Eric <ericgrobledo@gmail.com>
2020-08-24 23:30:23 -0500

    inicial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './img/tiendaFHD.png';
 import './App.css';
 
 function App() {
@@ -7,17 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          LA TIENDITA
+        </h1>
+        <h3>BIENVENIDOS</h3>
       </header>
     </div>
   );
